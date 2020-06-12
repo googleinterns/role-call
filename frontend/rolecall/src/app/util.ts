6e6882adcc75e2b8d64c812efcbb972ba898874d
Jagger Brulato <jaggerbrulato@google.com>
2020-06-12 14:17:08 -0400

    added comment for router url formatting util function

--- frontend/rolecall/src/app/util.ts
+++ frontend/rolecall/src/app/util.ts
@@ -1,3 +1,5 @@
+// Takes a router string and formats it to match
+// the router's internal url string
 export function cleanRouterString(input: string){
     if(input.endsWith('/') && input.length > 1){
         input = input.substr(0, input.length - 1);
