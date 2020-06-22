58b5e18fda1e12db9f229589f104ccdf430661cb
Jagger Brulato <jaggerbrulato@google.com>
2020-06-22 14:14:55 -0400

    renamed common module to /common as per FE structure

--- frontend/rolecall/src/app/homepage/homepage.module.ts
+++ frontend/rolecall/src/app/homepage/homepage.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageHeaderComponent } from './homepage_header.component';
 import { HomepageBaseComponent } from './homepage_base.component';
-import { CommonComponentsModule } from '../common_components/common_components.module';
+import { CommonComponentsModule } from '../common/common.module';
 
 
 
