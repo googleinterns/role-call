7c5cbc817f9ca3de81efa0a9de27a1e52a179d51
Jared Hirsch <jaredhirsch@google.com>
2020-07-24 20:48:25 +0000

    Fix test

--- backend/src/test/java/com/google/rolecall/config/WebSecurityUnitTests.java
+++ backend/src/test/java/com/google/rolecall/config/WebSecurityUnitTests.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.env.MockEnvironment;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +17,7 @@ public class WebSecurityUnitTests {
   @Test
   public void runWebSecurityConfigurations_success() throws Exception {
     // Setup
-    WebSecurityConfig config = new WebSecurityConfig(new MockEnvironment());
+    WebSecurityConfig config = new WebSecurityConfig();
     ObjectPostProcessor<Object> objectPostProcessor = new ObjectPostProcessor<Object>() {
       public <T> T postProcess(T object) {
         throw new IllegalStateException("For testing purposes only");
