ffec5d585be08b6c6940beaeef7e962ade013b8c
Jared Hirsch <jaredhirsch@google.com>
2020-08-05 01:47:58 +0000

    minor update

--- backend/src/main/java/com/google/rolecall/authentication/CustomOauthAuthenticationProvider.java
+++ backend/src/main/java/com/google/rolecall/authentication/CustomOauthAuthenticationProvider.java
@@ -47,7 +47,7 @@ public class CustomOauthAuthenticationProvider implements AuthenticationProvider
         throw new BadCredentialsException("Email and token do not validate with Google.");
       }
     } catch(Exception ex) {
-      logger.log(Level.SEVERE, ex.toString());
+      logger.log(Level.SEVERE, ex.getStackTrace().toString());
       throw new AuthenticationServiceException("Unable to successfully authenticate", ex);
     }
 
