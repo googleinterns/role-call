58e6cc5516eea83a32405723d5ea7d700290917a
Jared Hirsch <jaredhirsch@google.com>
2020-08-11 04:03:28 +0000

    Remove extra imports

--- backend/src/main/java/com/google/rolecall/config/RepositoryConfig.java
+++ backend/src/main/java/com/google/rolecall/config/RepositoryConfig.java
@@ -1,12 +1,8 @@
 package com.google.rolecall.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
