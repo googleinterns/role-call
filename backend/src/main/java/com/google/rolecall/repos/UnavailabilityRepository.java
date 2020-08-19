609605b7439baa7e0e443a6f48d1af5ce42a597c
Jared Hirsch <jaredhirsch@google.com>
2020-08-19 00:00:40 +0000

    Minor style change

--- backend/src/main/java/com/google/rolecall/repos/UnavailabilityRepository.java
+++ backend/src/main/java/com/google/rolecall/repos/UnavailabilityRepository.java
@@ -1,10 +1,6 @@
 package com.google.rolecall.repos;
 
-import java.sql.Date;
-import java.util.List;
-
 import com.google.rolecall.models.Unavailability;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface UnavailabilityRepository extends CrudRepository<Unavailability, Integer> {
