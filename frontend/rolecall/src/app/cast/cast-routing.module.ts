56275d278c26bdd46cff21014e614a9ba4b33f8f
yhedholm <69502014+yhedholm@users.noreply.github.com>
2020-09-29 14:03:10 -0700

    Removed the old unused cast editor files

--- frontend/rolecall/src/app/cast/cast-routing.module.ts
+++ frontend/rolecall/src/app/cast/cast-routing.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CastEditorV2 } from './cast-editor-v2.component';
-import { CastModule } from './cast.module';
-import { CastingEditor } from './casting_editor.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CastEditorV2} from './cast-editor-v2.component';
+import {CastModule} from './cast.module';
 
 
 const routes: Routes = [
-  { path: 'castv1/:uuid', component: CastingEditor },
-  { path: 'castv1', component: CastingEditor },
-  { path: 'cast', component: CastEditorV2 },
-  { path: 'cast/:uuid', component: CastEditorV2 },
+  {path: 'cast', component: CastEditorV2},
+  {path: 'cast/:uuid', component: CastEditorV2},
 ];
 
 @NgModule({
