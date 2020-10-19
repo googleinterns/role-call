591b117dc642f55426f1009248263dd55f8cc4be
yhedholm <69502014+yhedholm@users.noreply.github.com>
2020-10-19 11:11:02 -0700

    Backend updates

--- backend/src/test/java/com/google/rolecall/restcontrollers/UserManagementUnitTests.java
+++ backend/src/test/java/com/google/rolecall/restcontrollers/UserManagementUnitTests.java
@@ -42,7 +42,7 @@ public class UserManagementUnitTests {
   private String email = "goodEmail@gmail.com";
   private Calendar dateJoined = null;
   private Boolean isAdmin = true;
-  private Boolean isCoreographer = true;
+  private Boolean isChoreographer = true;
   private Boolean isDancer = true;
   private Boolean isOther = true;
   private Boolean canLogin = true;
