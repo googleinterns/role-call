f1465ed730d1acbfab5a6b6ec5b4afd3e439dfca
yhedholm <69502014+yhedholm@users.noreply.github.com>
2020-10-19 16:40:46 -0700

    Jared's last changes

--- backend/src/main/java/com/google/rolecall/restcontrollers/AsyncRestEndpoint.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/AsyncRestEndpoint.java
@@ -9,6 +9,13 @@ import org.springframework.security.authentication.RememberMeAuthenticationToken
 /* Utility methods for REST api enpoint calls. */
 public abstract class AsyncRestEndpoint {
 
+  /**
+   * Gets the User from the current principal. The principal is 
+   * expected to be a {@link RememberMeAuthenticationToken} from the session.
+   * The UserDetails field is excpected to be an instance of {@link CustomUserDetails}.
+   * 
+   * @return {@link User} user for the principal.
+   */
   public User getUser(Principal principal) {
     RememberMeAuthenticationToken token = (RememberMeAuthenticationToken) principal;
     CustomUserDetail userDetail = (CustomUserDetail) token.getPrincipal();
@@ -16,7 +23,14 @@ public abstract class AsyncRestEndpoint {
 
     return user;
   }
-
+  
+  /**
+   * Returns proper exception when the User does not have the required perssions to access
+   * an endpoint.
+   * 
+   * @param requiredPrivileges the privleges expected to use the endpoint.
+   * @return {@link ForbiddenException}
+   */
   public ForbiddenException insufficientPrivileges(String requiredPrivileges) {
     ForbiddenException failure = new ForbiddenException(
         String.format("Requires Permissions: %s", requiredPrivileges));
--- backend/src/main/java/com/google/rolecall/restcontrollers/UnavailabilityManagement.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/UnavailabilityManagement.java
@@ -12,6 +12,7 @@ import com.google.rolecall.restcontrollers.Annotations.Post;
 import com.google.rolecall.restcontrollers.exceptionhandling.RequestExceptions.EntityNotFoundException;
 import com.google.rolecall.restcontrollers.exceptionhandling.RequestExceptions.InvalidParameterException;
 import com.google.rolecall.services.UnavailabilityServices;
+
 import java.sql.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -19,11 +20,20 @@ import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/** Endpoints for manipulating Unavailability objects. */
 @Endpoint(Constants.Mappings.UNAVAILABILITY_MANAGEMENT)
 public class UnavailabilityManagement extends AsyncRestEndpoint {
 
   private final UnavailabilityServices unavailabilityService;
-  
+
+  /**
+   * Gets all {@link Unavailability} objects that overlap the provided date.
+   * 
+   * @param startLong is the start date in miliseconds since the epoch
+   * @param endLong is the end date in miliseconds since the epoch
+   * 
+   * @return List of {@link UnavailabilityInfo} objects.
+   */  
   @Get({Constants.RequestParameters.START_DATE, Constants.RequestParameters.END_DATE})
   public CompletableFuture<ResponseSchema<List<UnavailabilityInfo>>> getAllUnavailable(
       @RequestParam(value=Constants.RequestParameters.START_DATE, required=true) long startLong,
@@ -40,6 +50,15 @@ public class UnavailabilityManagement extends AsyncRestEndpoint {
     return CompletableFuture.completedFuture(response);
   }
 
+  /**
+   * Creates a new {@link Unavailability} object.
+   * 
+   * @param info is the provided information for a new {@link Unavailability} object
+   * 
+   * @return {@link UnavailabilityInfo} of the saved object.
+   * @throws InvalidParameterException if insufficienct properties are provided
+   * @throws EntityNotFoundException if userId provided has no valid {@link User} object.
+   */  
   @Post
   public CompletableFuture<ResponseSchema<UnavailabilityInfo>> createNewUnvailability(
       @RequestBody UnavailabilityInfo info) {
@@ -58,6 +77,15 @@ public class UnavailabilityManagement extends AsyncRestEndpoint {
     return CompletableFuture.completedFuture(response);
   }
 
+  /**
+   * Upadtes an existing {@link Unavailability} object.
+   * 
+   * @param info is the provided information for an existing {@link Unavailability} object
+   * 
+   * @return {@link UnavailabilityInfo} of the saved object.
+   * @throws InvalidParameterException if insufficienct properties are provided
+   * @throws EntityNotFoundException if id from info has no coresponding entity
+   */  
   @Patch
   public CompletableFuture<ResponseSchema<UnavailabilityInfo>> editUnvailability(
       @RequestBody UnavailabilityInfo info) {
@@ -76,6 +104,14 @@ public class UnavailabilityManagement extends AsyncRestEndpoint {
     return CompletableFuture.completedFuture(response);
   }
 
+  /**
+   * Deletes an existing {@link Unavailability} object.
+   * 
+   * @param id of the {@link Unavailability} object to be deleted
+   * 
+   * @throws InvalidParameterException if id is null
+   * @throws EntityNotFoundException if id has no coresponding entity
+   */  
   @Delete(Constants.RequestParameters.UNAVAILABLE_ID)
   public CompletableFuture<Void> deleteSection(@RequestParam(value=Constants.RequestParameters.UNAVAILABLE_ID, required=true) 
       int id) {
