7bed53a16803743b4168bf2ff98e3612d1cc0b60
RichardYang_2016 <richardyang@google.com>
2020-10-31 23:58:35 -0500

    adding qa env

---
+++ frontend/rolecall/src/environments/environment.qa.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  oauthClientID:
+    '295609371427-c10eqkgp6l7rhm7njnji72cqcm74uo4s.apps.googleusercontent.com',
+  mockBackend: false,
+  logRequests: false,
+  devEmail: 'admin@rolecall.com',
+  useDevEmail: false,
+  backendURL: 'https://backend-qa-dot-absolute-water-286821.uk.r.appspot.com/',
+};
