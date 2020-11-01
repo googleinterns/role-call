5a6fb0e0daccf0795ca74efa9fd939f05a0c53bf
RichardYang_2016 <richardyang@google.com>
2020-10-31 23:39:32 -0500

    change frondend endpoint url

--- frontend/rolecall/src/environments/environment.prod.ts
+++ frontend/rolecall/src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
-  oauthClientID: '295609371427-c10eqkgp6l7rhm7njnji72cqcm74uo4s.apps.googleusercontent.com',
+  oauthClientID:
+    '295609371427-c10eqkgp6l7rhm7njnji72cqcm74uo4s.apps.googleusercontent.com',
   mockBackend: false,
   logRequests: false,
   devEmail: 'admin@rolecall.com',
   useDevEmail: false,
-  backendURL: 'https://backend-dev-dot-absolute-water-286821.uk.r.appspot.com/'
+  backendURL:
+    'https://backend-prod-dot-absolute-water-286821.uk.r.appspot.com/',
 };
