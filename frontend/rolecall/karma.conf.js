b2fd64c92331f1c53d42e546c2ae21f923c94656
RichardYang_2016 <richardyang@google.com>
2020-11-16 19:19:29 -0600

    clear trailing space

--- frontend/rolecall/karma.conf.js
+++ frontend/rolecall/karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
-      }     
+      }
     },
     singleRun: false,
     restartOnFileChange: true
