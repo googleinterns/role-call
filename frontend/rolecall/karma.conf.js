68793405518867b17e47019a57fbc080823c0a05
RichardYang_2016 <richardyang@google.com>
2020-11-16 19:18:17 -0600

    remove headless chromium  from browser options

--- frontend/rolecall/karma.conf.js
+++ frontend/rolecall/karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome','ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
