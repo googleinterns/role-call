4c65e2d8e7a56448c506bbbb3bded619b3028735
WolfgangEdholm <66700542+WolfgangEdholm@users.noreply.github.com>
2022-08-18 01:16:54 -0700

    parameterized performance ux (#151)
    * parameterized performance ux
    
    * removed commented out code
    
    * removed more commented out code
    
    Co-authored-by: yhedholm <yedholm@gmail.com>

--- frontend/rolecall/src/app/common-components/perf-buttons.component.ts
+++ frontend/rolecall/src/app/common-components/perf-buttons.component.ts
@@ -6,14 +6,29 @@ export type Handler = () => void;
 export type ItemHandler = (ix: number) => void;
 
 @Component({
-  selector: 'app-action-buttons',
-  templateUrl: './action-buttons.component.html',
-  styleUrls: ['./action-buttons.component.scss']
+  selector: 'app-perf-buttons',
+  templateUrl: './perf-buttons.component.html',
+  styleUrls: ['./perf-buttons.component.scss']
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
-export class ActionButtonsComponent implements OnInit/*, OnChanges*/ {
+export class PerfButtonsComponent implements OnInit/*, OnChanges*/ {
 
   @Input() name = 'Object';
+
+  @Input() backIcon = 'arrow_back';
+  @Input() backName = 'Back';
+  @Input() backAria = 'Go to Previous Step';
+  @Input() backTip = '';
+  @Input() backActivator?: Activator;
+  @Input() backHandler?: Handler;
+
+
+  @Input() forwardIcon = 'arrow_forward';
+  @Input() forwardName = 'Forward';
+  @Input() forwardAria = 'Go to Next Step';
+  @Input() forwardTip = '';
+  @Input() forwardActivator?: Activator;
+  @Input() forwardHandler?: Handler;
 
   @Input() aIcon = 'delete';
   @Input() aName = 'Delete';
@@ -28,8 +43,6 @@ export class ActionButtonsComponent implements OnInit/*, OnChanges*/ {
   @Input() bTip = '';
   @Input() bActivator?: Activator;
   @Input() bHandler?: Handler;
-  @Input() bMenuItems = [];
-  @Input() bMenuHandler?: ItemHandler;
 
   @Input() cIcon = 'save';
   @Input() cName = 'Save';
@@ -41,12 +54,32 @@ export class ActionButtonsComponent implements OnInit/*, OnChanges*/ {
 
   // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
   ngOnInit(): void {
+    this.backAria = this.backAria || `Go to Previous Step`;
+    this.backTip = this.backTip || this.backAria;
+    this.forwardAria = this.forwardAria || `Go to Next Step`;
+    this.forwardTip = this.forwardTip || this.forwardAria;
     this.aAria = this.aAria || `${this.aName} ${this.name}`;
     this.aTip = this.aTip || this.aAria;
     this.bAria = this.bAria || `${this.bName} ${this.name}`;
     this.bTip = this.bTip || this.bAria;
     this.cAria = this.cAria || `${this.cName} ${this.name}`;
     this.cTip = this.cTip || this.cAria;
+  }
+
+  backActivate = (): boolean =>
+    this.backActivator ? this.backActivator() : true;
+
+
+  backHandle = (): void => {
+    if (this.backHandler) { this.backHandler(); }
+  }
+
+  forwardActivate = (): boolean =>
+    this.forwardActivator ? this.forwardActivator() : true;
+
+
+  forwardHandle = (): void => {
+    if (this.forwardHandler) { this.forwardHandler(); }
   }
 
   aActivate = (): boolean =>
@@ -65,12 +98,8 @@ export class ActionButtonsComponent implements OnInit/*, OnChanges*/ {
     if (this.bHandler) { this.bHandler(); }
   }
 
-  bMenuHandle = (ix: number): void => {
-    if (this.bMenuHandler) { this.bMenuHandler(ix); }
-  }
-
   cActivate = (): boolean =>
-    this.cActivator ? this.cActivator() : true;
+  this.cActivator ? this.cActivator() : true;
 
 
   cHandle = (): void => {
