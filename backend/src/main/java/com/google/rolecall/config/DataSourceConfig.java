b9ca46207076624101ce8dd92657d954ba05e1de
WolfgangEdholm <66700542+WolfgangEdholm@users.noreply.github.com>
2022-09-09 18:17:12 -0700

    Update DataSourceConfig.java (#160)
    * Update DataSourceConfig.java
    
    * Update response-status-handler.service.ts
    
    Co-authored-by: yhedholm <yedholm@gmail.com>

--- backend/src/main/java/com/google/rolecall/config/DataSourceConfig.java
+++ backend/src/main/java/com/google/rolecall/config/DataSourceConfig.java
@@ -95,6 +95,7 @@ public class DataSourceConfig {
     String projectId = env.getProperty("spring.cloud.gcp.projectId");
     String secretName = env.getProperty("cloud.secret.name");
     try {
+System.out.printf("getCloudDbPassword projectId=%s secretName=%s \n", projectId, secretName);
       password = getSecretResponse(projectId, secretName).getPayload().getData().toStringUtf8();
     } catch (IOException e) {
       throw new RuntimeException(
