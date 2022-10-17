efd866adc05bb6a8488fcd36dba181f64d60102e
yhedholm <yedholm@gmail.com>
2022-10-17 15:26:04 -0700

    qa-cache-dbg-01

--- frontend/rolecall/src/environments/environment.qa.ts
+++ frontend/rolecall/src/environments/environment.qa.ts
@@ -4,7 +4,8 @@ export const environment = {
   oauthClientID:
     '295609371427-c10eqkgp6l7rhm7njnji72cqcm74uo4s.apps.googleusercontent.com',
   mockBackend: true,
-  logRequests: false,
+  //logRequests: false,
+  logRequests: true,
   devEmail: 'admin@rolecall.com',
   useDevEmail: false,
   backendURL: 'https://backend-qa-dot-absolute-water-286821.uk.r.appspot.com/',
